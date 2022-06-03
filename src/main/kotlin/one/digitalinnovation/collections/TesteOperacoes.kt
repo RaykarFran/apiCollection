804bacc8a1f5155a24d1e026982fe8a5331169a1
RaykarFran <raykarfran@gmail.com>
2022-06-03 15:45:06 -0300

    Aula teste2, Array Operações

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -8,7 +8,22 @@ fun main() {
     }
 
     println ("--------------------")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salario: ${salarios.average()}")
+    
+
+    println ("--------------------")
     val salariosMaiorque2500 = salarios.filter { it > 2500.0}
     salariosMaiorque2500.forEach {println(it)}
 
+    println ("--------------------")
+    println(salarios.count { it in 2000.0..5000.0})
+
+    println ("--------------------")
+    println(salarios.find { it == 2250.0})
+
+    println ("--------------------")
+    println(salarios.any { it == 1000.0})
+    println(salarios.any { it == 500.0})
 }
